2d2d7337b07f764c054b0578f7b6d272289272fe
ChanHyuc <whk1140@gmail.com>
2024-10-16 23:23:46 +0900

    Init: Created BoxOffice Project

---
+++ BoxOffice/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  BoxOffice
+//
+//  Created by ChanHyuc on 10/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+
+    private func configureUI() {
+        view.backgroundColor = .systemBackground
+    }
+
+}
+
